070eb455305dd8324afa18483396500e3eac3f80
Matthew Schembri <mattsc98@gmail.com>
2019-10-29 18:02:23 +0100

    Initial Testing
    Starting the Initial before and after

--- src/test/java/edu/oum/currencymanager/currencies/CurrencyDatabaseTest.java
+++ src/test/java/edu/oum/currencymanager/currencies/CurrencyDatabaseTest.java
@@ -1,4 +1,20 @@
 package edu.oum.currencymanager.currencies;
+import org.junit.After;
+import org.junit.Before;
+import edu.uom.currencymanager.currencies.CurrencyDatabase;
 
 public class CurrencyDatabaseTest {
+
+    CurrencyDatabase currDB;
+
+    @Before
+    public void setup() throws Exception {
+        currDB = new CurrencyDatabase();
+    }
+
+    @After
+    public void teardown() {
+        currDB = null;
+    }
+
 }
